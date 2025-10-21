2c4afa0fd65476800bc35ad646fe64fa49cae76d
mateuscrd <mateuscrodrigues123@gmail.com>
2025-10-21 10:05:42 -0300

    Iniciando projeto

---
+++ src/main/java/br/edu/imepac/clinica/medica/ClinicaMedica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.imepac.clinica.medica;
+
+/**
+ *
+ * @author Mateus
+ */
+public class ClinicaMedica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
